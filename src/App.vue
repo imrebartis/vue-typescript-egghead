9d9da184a9fdf32db2657c3108c989f223c4022c
Imre Bartis <imre.bartis@siili.com>
2018-01-23 10:38:07 +0200

    watch for changes with @Watch decorator

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,51 @@
+<!-- The console.log works okay with this code:
+
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <MyCheckbox :title="checkbox.title" :value="checkbox.value" v-model="checkbox.checked"/>
+    <button @click="sumUp">Count {{count}}</button>
+  </div>
+</template>
+
+<script lang="ts">
+// import Vue from 'vue'
+// import { Component, Watch } from 'vue-property-decorator'
+
+// @Component
+// export default class App extends Vue {
+//   count: number = 0
+
+//   @Watch('count')
+//   watchCount(newVal, oldVal) {
+//     console.log('newVal: ', newVal, ', oldVal: ', oldVal)
+//   }
+
+//   sumUp() {
+//     this.count++
+//   }
+// }
+// </script> -->
+
+<!-- console.log shows incorrectly the acum of old & new value to be the same -->
+<template>
+  <div id="app">
+    <button @click="sumUp">Count {{count.acum}}</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { Component } from 'vue-property-decorator'
-import MyCheckbox from './components/MyCheckbox.vue'
-@Component({
-  components: {
-    MyCheckbox
-  }
-})
+import { Component, Watch } from 'vue-property-decorator'
+@Component
 export default class App extends Vue {
-  checkbox = {
-    title: 'Fancy checkbox',
-    value: 'checkbox-id',
-    checked: true
+  count: any = {
+    acum: 0
+  }
+  @Watch('count', { deep: true })
+  watchCount(newVal, oldVal) {
+    console.log('newVal: ', newVal.acum, ', oldVal: ', oldVal.acum)
+  }
+  sumUp() {
+    this.count.acum++
   }
 }
 </script>
@@ -36,5 +63,25 @@ export default class App extends Vue {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  font-size: 2em;
+}
+</style>
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button {
+  font-size: 2em;
 }
 </style>
